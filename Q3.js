235e2ccb15348272c798537dc97e7b5208fd5056
Zharakamkar <zahrakamkar203@gmail.com>
2023-02-02 05:05:49 +0330

    feature JS

---
+++ Q3.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+function unique(arr) {
+    return Array.from(new Set(arr));
+}
+console.log(unique(values))
